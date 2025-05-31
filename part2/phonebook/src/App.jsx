06e8e37c0b41f3912a13d5e7a25dcc76e2ca6ce1
Okputu, Okputu Emmanuel <immanuelokputu94@gmail.com>
2025-05-31 04:13:48 +0300

    phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import personService from "./services/Persons";
+import personService from "./services/persons";
 import "./index.css";
 
 const Notifications = ({ message, label }) => {
